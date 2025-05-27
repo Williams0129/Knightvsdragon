7b5e11367d66db1f66c261d5fad0706aedc8479e
Williams <d0968907129@gmail.com>
2025-05-27 20:47:41 +0800

    commit test

--- src/com/binge/Main.java
+++ src/com/binge/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(){
         int y=0;
         y+=1;
-        System.out.println("Im your dad");
+        System.out.println("Im your dad, yyyyyyyy");
     }
 }
